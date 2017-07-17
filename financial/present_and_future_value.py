d3d175575cd5487539ebcc6baad8d0f3e7a1fe73
Curtis Salisbury <csalisbury@artemishealth.com>
2017-07-17 11:34:06 -0600

    added more to the compound interest calculator

--- financial/present_and_future_value.py
+++ financial/present_and_future_value.py
@@ -3,14 +3,29 @@ Return the present or future value of money with a set interest rate
 """
 
 
-def future_or_present(future=False, principal=None, rate=None, time=None):
-    discount = (1 + rate) ** time
-    if future:
-        future_value = principal * discount
-        return future_value
-    else:
-        present_value = principal / discount
-        return present_value
+def TVM(p, r, t, type):
+    discount = (1 + r) ** t
+    if type == "FV":
+        A = p * discount
+        return A
+    if type == "PV":
+        A = p / discount
+        return A
 
-print(future_or_present(future=True, principal=100, rate=0.08, time=5))
-print(future_or_present(principal=100, rate=0.08, time=5))
+payments = [100, 200, 300, 400]
+years = [5, 10, 15, 20]
+rates = [.01, .02, .03, .04]
+
+
+def totalTVM(payments, years, rates):
+    total = 0
+    for index in range(0, len(payments)):
+        total += TVM(payments[index], years[index], rates[index], "PV")
+    return total
+
+
+
+total = 0
+for year in range(1, 1001):
+    total += TVM(100, .02, year, "PV")
+print(total)
