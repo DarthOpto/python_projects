275638d8217dc75789655300d948d1c054ee7163
Curtis Salisbury <csalisbury@artemishealth.com>
2017-08-07 10:24:31 -0600

    query git api for issues

--- web_services/retrieve_from_git/git_calls.py
+++ web_services/retrieve_from_git/git_calls.py
@@ -1,14 +1,21 @@
 BASE_URL = 'https://api.github.com/'
 HIGH_ISSUES = BASE_URL + \
-        'search/issues?q=repo:artemishealth/artemis-app+label:bug+label:prio:high+-label:customer_reported+state:open'
+        'search/issues?q=repo:artemishealth/artemis-app+label:bug+' \
+        'label:prio:high+-label:customer_reported+state:open&sort=created&order=asc'
 NORMAL_ISSUES = BASE_URL + \
-        'search/issues?q=repo:artemishealth/artemis-app+label:bug+label:prio:normal+-label:customer_reported+state:open'
+        'search/issues?q=repo:artemishealth/artemis-app+label:bug+' \
+        'label:prio:normal+-label:customer_reported+state:open&sort=created&order=asc'
 LOW_ISSUES = BASE_URL + \
-        'search/issues?q=repo:artemishealth/artemis-app+label:bug+label:prio:low+-label:customer_reported+state:open'
+        'search/issues?q=repo:artemishealth/artemis-app+label:bug+' \
+        'label:prio:low+-label:customer_reported+state:open&sort=created&order=asc'
 CUSTOMER_REPORTED_ISSUES = \
-    BASE_URL + 'search/issues?q=repo:artemishealth/artemis-app+label:bug+label:customer_reported+state:open'
-BLOCKER_ISSUES = BASE_URL + 'search/issues?q=repo:artemishealth/artemis-app+label:bug+label:prio:blocker+state:open'
+    BASE_URL + \
+    'search/issues?q=repo:artemishealth/artemis-app+label:bug+' \
+    'label:customer_reported+state:open&sort=created&order=asc'
+BLOCKER_ISSUES = BASE_URL + \
+    'search/issues?q=repo:artemishealth/artemis-app+label:bug+' \
+    'label:prio:blocker+-label:customer_reported+state:open&sort=created&order=asc'
 
 # TODO - Replace below with a token.
-USERNAME = 'MyUserName'
-PASSWORD = 'MyPassword'
+USERNAME = 'MY USER'
+PASSWORD = 'MY PASSWORD'
--- web_services/retrieve_from_git/git_issues.py
+++ web_services/retrieve_from_git/git_issues.py
@@ -11,12 +11,15 @@ ISSUE_CALLS = {'blocker': gc.BLOCKER_ISSUES,
 
 def issue_counts():
     results = []
+    issues = []
 
     for key, value in ISSUE_CALLS.items():
         request = requests.get(value, auth=(gc.USERNAME, gc.PASSWORD))
         parsed_response = request.json()
         number_of_issues = parsed_response.get('total_count')
-        results.append((key, number_of_issues))
+        issue = parsed_response.get('items')
+        issues.append((issue[1].get('number'), issue[1].get('created_at')))
+        results.append((key, number_of_issues, issues))
     return results
 
 
