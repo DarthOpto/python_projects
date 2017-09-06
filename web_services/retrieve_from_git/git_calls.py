9fb48705d335ef0f569d929549ecced06c005639
Curtis Salisbury <csalisbury@artemishealth.com>
2017-09-06 12:14:45 -0600

    query git api for issues

--- web_services/retrieve_from_git/git_calls.py
+++ web_services/retrieve_from_git/git_calls.py
@@ -1,6 +1,6 @@
 BASE_URL = 'https://api.github.com/'
 OPEN_ISSUES = BASE_URL + 'repos/artemishealth/artemis-app/issues?state=all&labels=bug&page={}'
-CUSTOMER_REPORTED = BASE_URL + 'repos/artemishealth/artemis-app/issues?state=all&labels=bug,customer_reported&page={}'
+CUSTOMER_REPORTED = BASE_URL + 'repos/artemishealth/artemis-app/issues?state=all&labels=bug,customer%20reported&page={}'
 
 
 # TODO - Replace below with a token.
