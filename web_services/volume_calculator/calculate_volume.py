2ff8b2df86daa7e052adf6d9bacc48511484292a
Curtis Salisbury <curtis.salisbury@gmail.com>
2018-02-22 22:47:23 -0700

    finished adding the rest of the volume methods.

--- web_services/volume_calculator/calculate_volume.py
+++ web_services/volume_calculator/calculate_volume.py
@@ -13,14 +13,29 @@ def volume_return(shape=None, volume=None):
 
 
 @app.route("/v1/<string:shape>/<int:measure>")
-def calculate_volume(shape=None, measure=None):
+@app.route("/v1/<string:shape>/<int:measure1>/<int:measure2>")
+def calculate_volume(shape=None, measure=None, measure1=None, measure2=None):
     volume = None
     if shape == "cube":
-        volume = formulas.cube_volume(measure)
+        volume = formulas.cube_volume(side=measure)
     elif shape == "sphere":
-        volume = formulas.sphere_volume(measure)
+        volume = formulas.sphere_volume(radius=measure)
+    elif shape == "dodecahedron":
+        volume = formulas.dodecahedron_volume(side=measure)
+    elif shape == "pyramid":
+        volume = formulas.right_square_pyramid(base_edge=measure1, height=measure2)
+    elif shape == "cylinder":
+        volume = formulas.cylinder_volume(radius=measure1, height=measure2)
 
     return volume_return(shape=shape, volume=volume)
 
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return str(json.dumps({"message": Exception})), 500
+
+
 if __name__ == "__main__":
     app.run(debug=False)
+
+# TODO - Add error handling that is displayed to the user
