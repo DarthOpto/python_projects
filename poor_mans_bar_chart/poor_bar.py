563e84f545a52b33b0e766c82d5dd4190f0928bf
Curtis Salisbury <curtis.salisbury@gmail.com>
2019-03-10 13:44:59 -0600

    Added a poor man's bar chart.

---
+++ poor_mans_bar_chart/poor_bar.py
@@ -0,0 +1,26 @@
+"""
+Poor Man's Bar Chart:
+
+Take any text and display the number of instances of each letter.
+"""
+import pprint
+from collections import defaultdict
+import string
+
+SENTENCE = input("Enter the text you would like to analyze: ").lower()
+TRANSLATOR1 = str.maketrans('', '', string.punctuation)
+TRANSLATOR2 = str.maketrans('', '', string.whitespace)
+WITHOUT_PUNCT = SENTENCE.translate(TRANSLATOR1)
+WITHOUT_WHITESPACE = WITHOUT_PUNCT.translate(TRANSLATOR2)
+
+s = list(WITHOUT_WHITESPACE)
+d = defaultdict(list)
+pp = pprint.PrettyPrinter()
+
+for k in s:
+    d[k].append(k)
+
+
+x = sorted(d.items())
+pp.pprint(x)
+
