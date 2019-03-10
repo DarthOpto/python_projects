92fc231430bda3458b04a973d7523f35b5f23071
Curtis Salisbury <curtis.salisbury@gmail.com>
2019-03-10 14:43:22 -0600

    Cleaned up the poor man's bar chart.

--- poor_mans_bar_chart/poor_bar.py
+++ poor_mans_bar_chart/poor_bar.py
@@ -5,21 +5,16 @@ Take any text and display the number of instances of each letter.
 """
 import pprint
 from collections import defaultdict
-import string
+from string import ascii_lowercase
 
 SENTENCE = input("Enter the text you would like to analyze: ").lower()
-TRANSLATOR1 = str.maketrans('', '', string.punctuation)
-TRANSLATOR2 = str.maketrans('', '', string.whitespace)
-WITHOUT_PUNCT = SENTENCE.translate(TRANSLATOR1)
-WITHOUT_WHITESPACE = WITHOUT_PUNCT.translate(TRANSLATOR2)
 
-s = list(WITHOUT_WHITESPACE)
 d = defaultdict(list)
 pp = pprint.PrettyPrinter()
 
-for k in s:
-    d[k].append(k)
-
+for letter in SENTENCE:
+    if letter in ascii_lowercase:
+        d[letter].append(letter)
 
 x = sorted(d.items())
 pp.pprint(x)
