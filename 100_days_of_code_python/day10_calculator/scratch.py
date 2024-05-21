b84055039fc95507b14c19b68b04b3ffd67e10ef
Curtis Salisbury <curtis.salisbury@gmail.com>
2024-05-21 09:59:09 -0600

    day 10

---
+++ 100_days_of_code_python/day10_calculator/scratch.py
@@ -0,0 +1,15 @@
+def format_name(f_name, l_name):
+    """
+    Take first and last name and format it to return
+    the title case version of the name.
+    """
+    if f_name == "" or l_name == "":
+        return "You didn't provide valid inputs."
+    first_name = f_name.title()
+    last_name = l_name.title()
+    return f"Result: {first_name} {last_name}"
+
+
+print(format_name(input("What is your first name? "), input("What is your last name? ")))
+
+
