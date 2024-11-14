c402632c239bd3437934756b2e2ebad0e76534a6
Curtis Salisbury <curtis.salisbury@gmail.com>
2024-11-14 11:38:42 -0700

    refactored code

--- python_bootcamp/todo/todo_console.py
+++ python_bootcamp/todo/todo_console.py
@@ -1,19 +1,12 @@
-def get_todos():
-    """
-    Reads the todos.txt file
-    Returns: a list of todo items
-    """
-    with open('files/todos.txt', 'r') as file:
+def get_todos(filepath):
+    with open(filepath, 'r') as file:
         todos_local = file.readlines()
     return todos_local
 
 
-def write_todos():
-    """
-    Writes todos to the todos.txt file
-    """
-    with open('files/todos.txt', 'w') as file:
-        file.writelines(todos)
+def write_todos(filepath, todos_to_write):
+    with open(filepath, 'w') as file:
+        file.writelines(todos_to_write)
 
 
 while True:
@@ -23,35 +16,35 @@ while True:
 
     if user_action.startswith('add'):
         todo = user_action[4:] + '\n'
-        todos = get_todos()
+        todos = get_todos('files/todos.txt')
         todos.append(todo)
-        write_todos()
+        write_todos('files/todos.txt', todos)
     elif user_action.startswith('edit'):
         try:
             number = int(user_action[5:])
             number -= 1
-            todos = get_todos()
+            todos = get_todos('files/todos.txt')
             new_todo = input('Enter new to-do: ')
             todos[number] = new_todo + '\n'
-            write_todos()
+            write_todos('files/todos.txt', todos)
         except ValueError:
             print('Your command is not valid. Enter the number of the to-do to edit')
             continue
     elif user_action.startswith('complete'):
         try:
             number = int(user_action[9:])
-            todos = get_todos()
+            todos = get_todos('files/todos.txt')
             index = number - 1
             todo_to_remove = todos[index].strip('\n')
             todos.pop(index)
-            write_todos()
+            write_todos('files/todos.txt', todos)
             message = f'Todo {todo_to_remove} was removed from the list.'
             print(message)
         except IndexError:
             print('Your command is not valid. Enter the number of the to-do to complete')
             continue
     elif user_action.startswith('show'):
-        todos = get_todos()
+        todos = get_todos('files/todos.txt')
 
         for index, item in enumerate(todos):
             item = item.strip('\n')
