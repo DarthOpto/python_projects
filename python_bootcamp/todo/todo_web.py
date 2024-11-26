8bdc3e90fd5a8f0e41c5c606e322b22ea8da1b63
Curtis Salisbury <curtis.salisbury@gmail.com>
2024-11-26 12:22:55 -0700

    Fixed issue
    Fixed issue where the text_input was not cleared after task was added

--- python_bootcamp/todo/todo_web.py
+++ python_bootcamp/todo/todo_web.py
@@ -8,10 +8,21 @@ def add_todo():
     todo_to_add = st.session_state["new_todo"] + "\n"
     todos.append(todo_to_add)
     write_todos(todos)
+    clear_text()
+
+
+def clear_text():
+    st.session_state.new_todo = ""
 
 
 st.title("My To-Do App")
 st.write("This is my to-do app to increase my productivity")
+
+st.text_input(label="Add To-Do",
+              label_visibility="hidden",
+              placeholder="Enter a new to-do here...",
+              on_change=add_todo,
+              key="new_todo")
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -20,9 +31,3 @@ for index, todo in enumerate(todos):
         write_todos(todos)
         del st.session_state[todo]
         st.rerun()
-
-st.text_input(label="Add To-Do",
-              label_visibility="hidden",
-              placeholder="Enter a new to-do here...",
-              on_change=add_todo,
-              key="new_todo")
