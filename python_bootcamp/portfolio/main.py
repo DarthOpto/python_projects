06cd4f7b60d5abbba819bd0575e1c13fdf0337f7
Curtis Salisbury <curtis.salisbury@gmail.com>
2024-11-27 10:39:17 -0700

    Update main.py

--- python_bootcamp/portfolio/main.py
+++ python_bootcamp/portfolio/main.py
@@ -6,9 +6,13 @@ col1, col2 = st.columns(2)
 with col1:
     st.image("images/photo.jpg", width=200)
 
+
 with col2:
     st.title("Curtis Salisbury")
     content = """Hi, I am Curtis. I have been doing automated and manual software quality assurance
      for 17 years. I am now branching out in hopes of becoming a Python developer. Below is a list of projects I have
      worked on, and a link to their source code."""
     st.info(content)
+
+content2 = """Below you can find some of the apps I have built in Python. Feel free to contact me."""
+st.write(content2)
