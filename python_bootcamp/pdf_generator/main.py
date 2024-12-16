dce6ae7c07e0742fe73f8c3c5421205db83daad6
Curtis Salisbury <curtis.salisbury@gmail.com>
2024-12-16 12:00:23 -0700

    added add_footer method
    Added an add_footer method to get rid of duplication.

--- python_bootcamp/pdf_generator/main.py
+++ python_bootcamp/pdf_generator/main.py
@@ -6,27 +6,29 @@ pdf.set_auto_page_break(auto=False, margin=0)
 
 data = pd.read_csv("topics.csv")
 
+
+def add_footer(line_breaks):
+    pdf.ln(line_breaks)
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+
 for index, row in data.iterrows():
     pdf.add_page()
     # Set Header
     pdf.set_font(family="Times", style="B", size=24)
-    pdf.set_text_color(100,100,100)
+    pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
     pdf.line(10, 20, 200, 20)
 
     # Footer
-    pdf.ln(265)
-    pdf.set_font(family="Times", style="I", size=8)
-    pdf.set_text_color(180, 180, 180)
-    pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+    add_footer(265)
 
     for i in range(row["Pages"] - 1):
         pdf.add_page()
 
         # Footer
-        pdf.ln(277)
-        pdf.set_font(family="Times", style="I", size=8)
-        pdf.set_text_color(180, 180, 180)
-        pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+        add_footer(277)
 
 pdf.output("output.pdf")
