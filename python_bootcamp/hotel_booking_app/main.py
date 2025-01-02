4a01f59ce3673aeb1c3297bb77defea73e461ad1
Curtis Salisbury <curtis.salisbury@gmail.com>
2025-01-02 10:53:24 -0700

    oop

---
+++ python_bootcamp/hotel_booking_app/main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+hotels = pd.read_csv("hotels.csv")
+class Hotel:
+    def book(self):
+        pass
+
+
+class Reservation:
+    def generate_ticket(self):
+        pass
+
+
+print(hotels)
