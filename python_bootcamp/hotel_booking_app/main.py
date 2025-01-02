aa0a5c7c74903347605447087fa27f1e7c8c50b8
Curtis Salisbury <curtis.salisbury@gmail.com>
2025-01-02 11:31:11 -0700

    more oop

--- python_bootcamp/hotel_booking_app/main.py
+++ python_bootcamp/hotel_booking_app/main.py
@@ -1,14 +1,49 @@
 import pandas as pd
 
-hotels = pd.read_csv("hotels.csv")
+hotels = pd.read_csv("hotels.csv", dtype={"id": str})
+
+
 class Hotel:
+    def __init__(self, hotel_id):
+        self.hotel_id = hotel_id
+        self.hotel_name = hotels.loc[hotels["id"] == self.hotel_id, "name"].squeeze()
+
     def book(self):
-        pass
+        """Book a hotel by changing its availability to no"""
+        hotels.loc[hotels["id"] == self.hotel_id, "available"] = "no"
+        hotels.to_csv("hotels.csv", index=False)
+
+    def available(self):
+        """Checks if the hotel is available"""
+        availability = hotels.loc[hotels["id"] == self.hotel_id, "available"].squeeze()
+        if availability == "yes":
+            return True
+        else:
+            return False
 
 
 class Reservation:
-    def generate_ticket(self):
-        pass
+    def __init__(self, customer_name, hotel_object):
+        self.customer_name = customer_name
+        self.hotel = hotel_object
+
+    def generate_reservation(self):
+        content = f"""Thank you for your reservation!
+        Here is your booking data:
+        Name: {self.customer_name}
+        Hotel Name: {self.hotel.hotel_name}
+        """
+        return content
 
 
 print(hotels)
+hotel_ID = input("Enter the ID of the hotel: ")
+hotel = Hotel(hotel_ID)
+
+if hotel.available():
+    hotel.book()
+    name = input("Enter your name: ")
+    reservation_ticket = Reservation(customer_name=name, hotel_object=hotel)
+    print(reservation_ticket.generate_reservation())
+else:
+    print("Hotel has no availability")
